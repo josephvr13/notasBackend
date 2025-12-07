d22e9dc4aff1bfe4453bac371e8ec5c7858a40a6
josephvr13 <josephvera276@gmail.com>
2025-12-06 19:00:45 -0500

    Add __repr__ method to Nota class
    Added __repr__ method for Nota class to improve string representation.

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, func
-from database import Base
+from database import Base  # Asegúrate que database.py está en la misma carpeta
 
 class Nota(Base):
     __tablename__ = "notas"
@@ -8,3 +8,6 @@ class Nota(Base):
     titulo = Column(String(200), nullable=False)
     contenido = Column(Text, nullable=False)
     creado_en = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"<Nota id={self.id} titulo={self.titulo}>"
