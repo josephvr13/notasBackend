d0ccfaeeab7a72a1413c67b453563724ebd9cefb
josephvr13 <josephvera276@gmail.com>
2025-12-06 19:57:47 -0500

    Refactor database connection setup
    Removed default MySQL connection settings and simplified engine creation.

--- database.py
+++ database.py
@@ -4,14 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""
-    MYSQL_HOST = "127.0.0.1"
-    MYSQL_PORT = "3306"
-    MYSQL_DB = "notas_db"
-    DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+engine = create_engine(DATABASE_URL)
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
